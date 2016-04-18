a9467f2dd7503e59ab6c3e0d2716085f67a120cc
Donald Gray <donald.gray@maclean.co.uk>
2016-04-18 15:34:31 +0100

    Updates to assemblyInfo

--- src/ServiceStack.Configuration.Consul/ServiceStack.Configuration.Consul/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Configuration.Consul/ServiceStack.Configuration.Consul/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ServiceStack.Configuration.Consul")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implementation if ServiceStack's IAppSettings interface using Consul Key/Value as backing store.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MacLean Electrical")]
 [assembly: AssemblyProduct("ServiceStack.Configuration.Consul")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
--- test/ServiceStack.Configuration.Consul.Tests/Properties/AssemblyInfo.cs
+++ test/ServiceStack.Configuration.Consul.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ServiceStack.Configuration.Consul.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit tests for ServiceStack.Configuration.Consul project.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MacLean Electrical")]
 [assembly: AssemblyProduct("ServiceStack.Configuration.Consul.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]

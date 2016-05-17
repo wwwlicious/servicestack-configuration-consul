8a756aa00d9e311c1b03dfa54514778805c25c73
Donald Gray <donald.gray@maclean.co.uk>
2016-05-17 07:59:01 +0100

    Added keyUtilities class

---
+++ src/ServiceStack.Configuration.Consul/KeyUtilities.cs
@@ -0,0 +1,44 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this 
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+namespace ServiceStack.Configuration.Consul
+{
+    using System.Collections.Generic;
+
+    public class KeyUtilities
+    {
+        public static string GetDefaultLookupKey(string key) => $"ss/{key}";
+
+        /// <summary>
+        /// For specified key gets a list of all possible locations from most -> least specific
+        /// </summary>
+        /// <param name="key">Default key</param>
+        /// <returns>List of keys where value may be found (most to least specific)</returns>
+        public static IEnumerable<string> GetPossibleKeys(string key)
+        {
+            var defaultKey = GetDefaultLookupKey(key);
+
+            var appHost = HostContext.AppHost;
+
+            if (appHost == null)
+                return new[] { defaultKey };
+
+            var serviceName = appHost.ServiceName;
+            var serviceKey = $"ss/{key}/{serviceName}";
+
+            if (appHost.Config == null)
+                return new[] { serviceKey, defaultKey };
+
+            var version = appHost.Config.ApiVersion;
+
+            // TODO Instance specific key
+            return new[]
+            {
+                $"{serviceKey}/{version}", // version specific (ss/keyname/service/v1)
+                serviceKey, // service specific (ss/keyname/service)
+                defaultKey // default (ss/keyname)
+            };
+        }
+    }
+}
---
+++ test/ServiceStack.Configuration.Consul.Tests/KeyUtilitiesTests.cs
@@ -0,0 +1,74 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this 
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+namespace ServiceStack.Configuration.Consul.Tests
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using Testing;
+    using Xunit;
+
+    [Collection("KeyUtilitiesTests")]
+    public class KeyUtilitiesTests : IDisposable
+    {
+        private ServiceStackHost appHost;
+        private const string Key = "findMe";
+        private const string ServiceName = "testService";
+
+        [Fact]
+        public void GetPossibleKeys_ValuesInCorrectOrder()
+        {
+            Init();
+            var values = KeyUtilities.GetPossibleKeys(Key).ToList();
+            values.Count.Should().Be(3);
+            values[0].Should().Be($"ss/{Key}/{ServiceName}/1.0");
+            values[1].Should().Be($"ss/{Key}/{ServiceName}");
+            values[2].Should().Be($"ss/{Key}");
+        }
+
+        public void Dispose() => appHost?.Dispose();
+
+        private void Init()
+        {
+            if (ServiceStackHost.Instance == null)
+            {
+                appHost = new BasicAppHost { TestMode = true, ServiceName = ServiceName };
+                appHost.Init();
+            }
+        }
+    }
+
+    [Collection("KeyUtilitiesTests")]
+    public class KeyLookupUtilitiesHostlessTests : IDisposable
+    {
+        private readonly AppDomain noHost;
+
+        public KeyLookupUtilitiesHostlessTests()
+        {
+            noHost = AppDomain.CreateDomain("NoAppHost", AppDomain.CurrentDomain.Evidence,
+                AppDomain.CurrentDomain.SetupInformation);
+        }
+
+        [Fact]
+        public void GetPossibleKeys_NullAppHost_ReturnsDefault()
+        {
+            const string key = "findMe";
+            noHost.DoCallBack(() =>
+            {
+                var values = KeyUtilities.GetPossibleKeys(key);
+                values.Count().Should().Be(1);
+                values.First().Should().Be($"ss/{key}");
+            });
+        }
+
+        public void Dispose()
+        {
+            if (noHost != null)
+            {
+                AppDomain.Unload(noHost);
+            }
+        }
+    }
+}
